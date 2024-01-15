c8ed7b3551791d499599dedac00fdd23498ea123
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2024-01-14 17:21:24 -0800

    ch2-ch3: basic

---
+++ packt-chapter2/PacktChap2.java
@@ -0,0 +1,10 @@
+public class PacktChap2 {
+    public static void main(String[] args) throws Exception {
+        String breed = "Tyrannosaurus rex";
+        int height = 20; // Feet
+        int length = 40;
+        int weight = 10000; // lbs
+
+        System.out.printf("Breed: %s, Height: %d feet, Length: %d, Weight: %d lbs%n", breed, height, length, weight);
+    }
+}
